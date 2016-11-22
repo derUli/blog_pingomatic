de84d043ea63d48f7eae4ebf335db4403ce6fa0e
Ulrich Schmidt <admin@deruli.de>
2016-11-22 21:14:27 +0100

    modul erstellt

---
+++ src/content/modules/blog_pingomatic/lang/de.php
@@ -0,0 +1,8 @@
+<?php
+define ( "TRANSLATION_BLOG2TWITTER_ADMIN_HEADLINE", "Einstellungen von blog2twitter" );
+define ( "TRANSLATION_TWITTER_CONSUMER_KEY", "Twitter Consumer Key" );
+define ( "TRANSLATION_TWITTER_CONSUMER_SECRET", "Twitter Consumer Secret" );
+define ( "TRANSLATION_TWITTER_ACCESS_TOKEN", "Twitter Access Token" );
+define ( "TRANSLATION_TWITTER_ACCESS_TOKEN_SECRET", "Twitter Access Token Secret" );
+define ( "TRANSLATION_BLOG2TWITTER_WORKS", "Es funktioniert!" );
+define ( "TRANSLATION_BLOG2TWITTER_STATUS", "blog2twitter Status" );
---
+++ src/content/modules/blog_pingomatic/lang/en.php
@@ -0,0 +1,8 @@
+<?php
+define ( "TRANSLATION_BLOG2TWITTER_ADMIN_HEADLINE", "Settings of blog2twitter" );
+define ( "TRANSLATION_TWITTER_CONSUMER_KEY", "Twitter Consumer Key" );
+define ( "TRANSLATION_TWITTER_CONSUMER_SECRET", "Twitter Consumer Secret" );
+define ( "TRANSLATION_TWITTER_ACCESS_TOKEN", "Twitter Access Token" );
+define ( "TRANSLATION_TWITTER_ACCESS_TOKEN_SECRET", "Twitter Access Token Secret" );
+define ( "TRANSLATION_BLOG2TWITTER_WORKS", "It Works!" );
+define ( "TRANSLATION_BLOG2TWITTER_STATUS", "blog2twitter Status" );
