ed7b1e5910b7c673fbffca4360ce37cc75fa1460
Der Uli im Netz <admin@deruli.de>
2020-06-27 13:10:42 +0200

    update

--- src/content/modules/blog2pingomatic/hooks/cron.php
+++ src/content/modules/blog2pingomatic/hooks/cron.php
@@ -1,31 +1,6 @@
 <?php
 
-if (containsModule(null, "blog")) {
-    if (!function_exists("rootDirectory")) {
-
-        function rootDirectory() {
-            $pageURL = 'http';
-            if ($_SERVER ["HTTPS"] == "on") {
-                $pageURL .= "s";
-            }
-            $pageURL .= "://";
-            $dirname = dirname($_SERVER ["REQUEST_URI"]);
-            $dirname = str_replace("\\", "/", $dirname);
-            $dirname = trim($dirname, "/");
-            if ($dirname != "") {
-                $dirname = "/" . $dirname . "/";
-            } else {
-                $dirname = "/";
-            }
-            if ($_SERVER ["SERVER_PORT"] != "80") {
-                $pageURL .= $_SERVER ["SERVER_NAME"] . ":" . $_SERVER ["SERVER_PORT"] . $dirname;
-            } else {
-                $pageURL .= $_SERVER ["SERVER_NAME"] . $dirname;
-            }
-            return $pageURL;
-        }
-
-    }
+if (containsModule(null, "blog")) {       
     $query = db_query("select id, title, seo_shortname from " . tbname("blog") . " where entry_enabled = 1 and posted2pingomatic = 0 order by datum limit 2");
     if (db_num_rows($query) > 0) {
         while ($row = db_fetch_assoc($query)) {
@@ -33,7 +8,7 @@ if (containsModule(null, "blog")) {
             $title = $row ["title"];
             $seo_shortname = $row ["seo_shortname"];
 
-            $link = rootDirectory() . get_requested_pagename() . ".html?single=" . $seo_shortname;
+            $link = rootDirectory() . get_slug() . ".html?single=" . $seo_shortname;
 
             $post = $title . " " . $link;
             $controller = ControllerRegistry::get("PingomaticController");
